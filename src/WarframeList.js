a742bfe3ded108df00e3608b68c5f7fabfb7ca8d
Nithirex <nithirex@gmail.com>
2025-01-05 13:30:25 +0000

    Added proper fetch support rather that importing an inbuilt json

--- src/WarframeList.js
+++ src/WarframeList.js
@@ -1,21 +1,35 @@
 import { Link } from 'react-router-dom/cjs/react-router-dom'
-import warframeinfolist from './info.json'
+import { useEffect, useState } from 'react'
 
 const WarframeList = () => {
-    if (warframeinfolist[warframeinfolist.length - 1].name === "WARFRAME_PLACEHOLDER") {
-        warframeinfolist.pop()
-    }
+    const [warframeinfolist, setWarframeinfolist] = useState(null)
+
+    useEffect(() => {
+        const fetchWarframes = async () => {
+            const response = await fetch("http://localhost:8080/api/warframes")
+            const json = await response.json()
+            if (response.ok) {
+                setWarframeinfolist(json)
+            }
+        }
+
+        fetchWarframes()
+    }, [])
+
     return (
         <div className="warframe-list">
-            {warframeinfolist.map((warframeinfo) => (
-                ((warframeinfo.primed !== false)
-                    &&
-                    <div className="warframe-info" key={warframeinfo.name}>
-                        <Link to={'/warframestats/' + warframeinfo.name} >
-                            <h2>{warframeinfo.name}</h2>
-                        </Link>
-                    </div>)
-            ))}
+            {Boolean(warframeinfolist) &&
+                warframeinfolist.map((warframeinfo) => (
+                    ((warframeinfo.primed !== undefined && warframeinfo.name !== "WARFRAME_PLACEHOLDER")
+                        &&
+                        <div className="warframe-info" key={warframeinfo.name}>
+                            <Link to={'/warframestats/' + warframeinfo.name} >
+                                <h2>{warframeinfo.name}</h2>
+                            </Link>
+                        </div>
+                    )
+                ))
+            }
         </div >
     );
 }
